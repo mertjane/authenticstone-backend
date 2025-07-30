0f237d607f880b4097bdc3f3f0b4fbc3bfa2bb5e
unknown <mck0391@gmail.com>
2025-07-31 00:20:54 +0100

    dev version 0.2.4

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 
 const corsOptions = {
   /* origin: 'http://localhost:5173', // Development URL */
-  origin: 'https://9626d4be.authenticstone-frontend.pages.dev', // Production URL
+  origin: 'https://c8f8f2c6.authenticstone-frontend.pages.dev', // Production URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
