eb75b0284dcc78150ce66b0b4302943b17fe2999
unknown <mck0391@gmail.com>
2025-08-02 10:43:01 +0100

    dev commit v1.0.0

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const app = express();
 
 const corsOptions = {
   origin: [
-    'http://localhost:5173', // Development URL
-    'https://authenticstone-frontend.pages.dev', // Production URL
+    /* 'http://localhost:5173', */ // Development URL
+    'https://f6eb6e75.authenticstone-frontend.pages.dev', // Production URL
     process.env.FRONTEND_URL // Use environment variable as fallback
   ].filter(Boolean), // Remove any undefined values
   credentials: true,
